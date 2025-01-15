dd3efbc7c59f90cb2050c64c8a5ee09f760046ab
matix329 <mateusz.siejo@wp.pl>
2025-01-16 00:27:44 +0100

    Rename and update preprocessing script

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -7,8 +7,8 @@ class DataLoader:
         self.num_classes = num_classes
 
     def load(self, image_dir, mask_dir):
-        image_paths = tf.data.Dataset.list_files(image_dir + "/*/*.jpg", shuffle=True)
-        mask_paths = tf.data.Dataset.list_files(mask_dir + "/*/*.png", shuffle=True)
+        image_paths = tf.data.Dataset.list_files(image_dir + "/*.png", shuffle=True)
+        mask_paths = tf.data.Dataset.list_files(mask_dir + "/*.png", shuffle=True)
 
         dataset = tf.data.Dataset.zip((image_paths, mask_paths))
         dataset = dataset.map(self.process_path, num_parallel_calls=tf.data.AUTOTUNE)
@@ -23,7 +23,7 @@ class DataLoader:
 
     def load_image(self, image_path):
         image = tf.io.read_file(image_path)
-        image = tf.image.decode_jpeg(image, channels=3)
+        image = tf.image.decode_png(image, channels=3)
         image = tf.image.resize(image, self.image_size) / 255.0
         return image
 
@@ -32,5 +32,4 @@ class DataLoader:
         mask = tf.image.decode_png(mask, channels=1)
         mask = tf.image.resize(mask, self.image_size, method="nearest")
         mask = tf.cast(mask, tf.int32)
-        mask = tf.clip_by_value(mask, 0, self.num_classes - 1)
         return mask
