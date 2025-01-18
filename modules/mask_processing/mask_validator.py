2cc4992d9454e3436481cce433e64429350991e9
matix329 <mateusz.siejo@wp.pl>
2025-01-18 11:47:44 +0100

    Remove modules

--- modules/mask_processing/mask_validator.py
+++
@@ -1,5 +0,0 @@
-class MaskValidator:
-    @staticmethod
-    def validate_masks(images, masks):
-        for (img_name, img), (mask_name, mask) in zip(images, masks):
-            assert img.shape[1:] == mask.shape, f"Size error: {img_name} vs {mask_name}"
--- modules/preprocessing/normalizer.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-class DataNormalizer:
-    @staticmethod
-    def normalize(image):
-        return (image - np.min(image)) / (np.max(image) - np.min(image))
