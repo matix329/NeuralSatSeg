0d0e0bf89d173083d6a776d0cb6789f871fb627e
matix329 <mateusz.siejo@wp.pl>
2025-01-22 00:34:30 +0100

    Update data loader

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -7,29 +7,39 @@ class DataLoader:
         self.num_classes = num_classes
 
     def load(self, image_dir, mask_dir):
-        image_paths = tf.data.Dataset.list_files(image_dir + "/*.tif", shuffle=True)
-        mask_paths = tf.data.Dataset.list_files(mask_dir + "/*.tif", shuffle=True)
-
+        image_paths = tf.data.Dataset.list_files(f"{image_dir}/*.png", shuffle=True)
+        mask_paths = tf.data.Dataset.list_files(f"{mask_dir}/*.png", shuffle=True)
         dataset = tf.data.Dataset.zip((image_paths, mask_paths))
-        dataset = dataset.map(self.process_path, num_parallel_calls=tf.data.AUTOTUNE)
+
+        def process(image_path, mask_path):
+            image = self.load_image(image_path)
+            mask = self.load_mask(mask_path)
+            return image, mask
+
+        dataset = dataset.map(process, num_parallel_calls=tf.data.AUTOTUNE)
         dataset = dataset.batch(self.batch_size).prefetch(tf.data.AUTOTUNE)
-
         return dataset
-
-    def process_path(self, image_path, mask_path):
-        image = self.load_image(image_path)
-        mask = self.load_mask(mask_path)
-        return image, mask
 
     def load_image(self, image_path):
         image = tf.io.read_file(image_path)
-        image = tf.image.decode_png(image, channels=3)
+        image = tf.image.decode_image(image, channels=3, expand_animations=False)
         image = tf.image.resize(image, self.image_size) / 255.0
+        image = tf.ensure_shape(image, [self.image_size[0], self.image_size[1], 3])
         return image
 
     def load_mask(self, mask_path):
         mask = tf.io.read_file(mask_path)
         mask = tf.image.decode_png(mask, channels=1)
-        mask = tf.image.resize(mask, self.image_size, method="nearest")
-        mask = tf.cast(mask, tf.int32)
+        mask = tf.image.resize(mask, self.image_size)
+        mask = tf.ensure_shape(mask, [self.image_size[0], self.image_size[1], 1])
+        mask = tf.math.round(mask)
         return mask
+
+    def process_path(self, image_path, mask_path):
+        image = tf.py_function(func=self.load_image, inp=[image_path], Tout=tf.float32)
+        mask = tf.py_function(func=self.load_mask, inp=[mask_path], Tout=tf.float32)
+
+        image.set_shape([self.image_size[0], self.image_size[1], 3])
+        mask.set_shape([self.image_size[0], self.image_size[1], 1])
+
+        return image, mask
