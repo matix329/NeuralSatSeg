4673ac82fa151aba422fb332659471ae479d8942
matix329 <mateusz.siejo@wp.pl>
2025-05-13 19:13:50 +0200

    Add configuration options

--- training_models/config.py
+++ training_models/config.py
@@ -1,7 +1,22 @@
-CONFIG = {
+BUILDING_MASK_OPTIONS = ["masks_original", "masks_eroded"]
+ROAD_MASK_OPTIONS = ["masks_binary", "masks_graph"]
+
+SUPPORTED_MODELS = ["unet", "cnn"]
+
+DATA_DIR = "data/processed"
+OUTPUT_DIR = "output/mlflow_artifacts/models"
+LOG_DIR = "data/logs"
+
+TRAINING_CONFIG = {
     "input_shape": (512, 512, 3),
     "num_classes": 1,
     "batch_size": 16,
     "epochs": 30,
     "learning_rate": 0.0001,
+}
+
+MASK_EXTENSIONS = {
+    ".npy": "numpy",
+    ".pt": "torch",
+    ".png": "image"
 }
